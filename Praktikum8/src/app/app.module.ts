0a46969adc389f46e682b7a2e2532f5872b9f923
bamzdsptd <43591547+bamzdsptd@users.noreply.github.com>
2019-05-08 19:05:54 +0700

    js 6 dan js 8

---
+++ Praktikum8/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
